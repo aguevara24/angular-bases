1151a253bcba122536a39af81b496c61bb961d06
Alfredo Guevara Fuentes <aguevara24@hotmail.com>
2024-02-15 14:23:12 -0600

    Docs creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from "./counter/counter.module";
-import { HeroesModule } from "./heroes/heroes.module";
 import { DbzModule } from "./dbz/dbz.module";
+import { HeroesModule } from "./heroes/heroes.module";
 
 import { AppComponent } from './app.component';
 
@@ -15,8 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
+    DbzModule,
     HeroesModule,
-    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
